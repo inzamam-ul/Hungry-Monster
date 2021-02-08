dbc3386b853321ca69bcaa85f10ed925a71aefea
Inzamamul haque <inzamam.cu@gamil.com>
2021-02-08 13:53:52 +0800

    app.js updated

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const searchFood = (event) => {
 
         input.style.border = "1px solid green";
 
+        document.getElementById('singleFood').innerHTML = ""; 
+
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
             .then(res => res.json())
             .then(data => {
@@ -45,11 +47,11 @@ const searchFood = (event) => {
                 console.log(error);
                 let misingFood = input.value;
                 document.getElementById('allFoods').innerHTML = `
-                <div class="col d-flex flex-column justify-content-center align-items-center mt-5">
-                    <img class="error" src="images/icon.png" alt="">
-                    <h2 class="mt-3 error-msg">Sorry! "${misingFood} "Not found</h2>
-                </div>
-            `;
+                    <div class="col d-flex flex-column justify-content-center align-items-center mt-5">
+                        <img class="error" src="images/icon.png" alt="">
+                        <h2 class="mt-3 error-msg">Sorry! "${misingFood} "Not found</h2>
+                    </div>
+                `;
                 input.value = "";
                 document.getElementById('singleFood').innerHTML = "";
             });
@@ -69,27 +71,37 @@ const showDetails = id => {
             let singleFood = document.getElementById('singleFood');
             const meal = data.meals[0];
             const detailElement = `
-            <div class="card">
-               <button onclick="hide()" class="btn btn-primary">X</button>
-                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h3 class="card-title mt-2">${meal.strMeal}</h3>
-                    <h4 class="mt-2">Ingredients</h4>
-                    <ul class="mt-1">
-                        <li><span>${meal.strMeasure1}</span> <span>${meal.strIngredient1}</span></li>
-                        <li><span>${meal.strMeasure2}</span> <span>${meal.strIngredient2}</span></li>
-                        <li><span>${meal.strMeasure3}</span> <span>${meal.strIngredient3}</span></li>
-                        <li><span>${meal.strMeasure4}</span> <span>${meal.strIngredient4}</span></li>
-                        <li><span>${meal.strMeasure5}</span> <span>${meal.strIngredient5}</span></li>
-                        <li><span>${meal.strMeasure6}</span> <span>${meal.strIngredient6}</span></li>
-                        <li><span>${meal.strMeasure7}</span> <span>${meal.strIngredient7}</span></li>
-                
+                <div class="card">
+                <button onclick="hide()" class="btn btn-success close">X</button>
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title mt-2">${meal.strMeal}</h3>
+                        <h4 class="mt-2">Ingredients</h4>
+                        <ul class="mt-1">
+                            <li><span>${meal.strMeasure1}</span> <span>${meal.strIngredient1}</span></li>
+                            <li><span>${meal.strMeasure2}</span> <span>${meal.strIngredient2}</span></li>
+                            <li><span>${meal.strMeasure3}</span> <span>${meal.strIngredient3}</span></li>
+                            <li><span>${meal.strMeasure4}</span> <span>${meal.strIngredient4}</span></li>
+                            <li><span>${meal.strMeasure5}</span> <span>${meal.strIngredient5}</span></li>
+                            <li><span>${meal.strMeasure6}</span> <span>${meal.strIngredient6}</span></li>
+                            <li><span>${meal.strMeasure7}</span> <span>${meal.strIngredient7}</span></li>
+                    
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        `;
+            `;
             singleFood.innerHTML = detailElement;
+        })
+        .catch(error => {
+            console.log(error);
+            singleFood.innerHTML = `
+                    <div class="col d-flex flex-column justify-content-center align-items-center mt-5">
+                        <img class="error" src="images/icon.png" alt="">
+                        <h2 class="mt-3 error-msg">Sorry! "${meal.strMeal} " Details Not found</h2>
+                    </div>
+                `;
+
         })
 };
 
